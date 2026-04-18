25b8047ebed692c7cfdc03770ad38026942256ce
nitesh-1419 <niteshcounder042@gmail.com>
2026-04-18 08:33:25 +0530

    Delete netflix_clone directory

--- netflix_clone/lib/api_key.dart
+++
@@ -1,1 +0,0 @@
-const String tmdbApiKey = '481848478335606ffa9a0bc717b9a5fd';
--- netflix_clone/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-const String tmdbBaseUrl = 'https://api.themoviedb.org/3';
-const double kHeight10 = 10.0;
-const double kHeight20 = 20.0;
